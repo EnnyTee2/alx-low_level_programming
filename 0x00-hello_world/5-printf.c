69b3e9f07bbc0d7502b9a18b352f9bab5631a1ff
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-08 05:03:52 -0700

    latest changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  * Description: prints "Programming is like building a multilingual puzzle"
- * Return: Always 0 (Success)                               
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-{  
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
