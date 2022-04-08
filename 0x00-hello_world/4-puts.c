381175365572d244c70690f07a8e36d1b514e927
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-08 04:40:00 -0700

    latest changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/* 
+
+/** 
  * This program prints a text using the puts function
-*/
+ */
 
 
-int main(void) {
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
