adf176a73ba3dea14514397173478a61860da277
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-11 20:48:07 -0700

    new updates

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing single digit numbers of base 10 (0 - 9)
+ *
+ * Description: as above
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	for (int x = 0; x < 10; x++)
+	{
+		printf("%d\n", x);
+	}
+
+	return (0);
+}
