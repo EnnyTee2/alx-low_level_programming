c876411a2db6b6f0dcb5a4971619d4409c0651b3
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-11 21:37:55 -0700

    new updates

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combo of 2 digits
+ *
+ * Description: prints the double combo with the 2 digits different
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		
+		i++;
+	}
+
+	putchar(10);
+	return (0);
+}
