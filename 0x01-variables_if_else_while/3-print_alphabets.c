dcaa307e19dc80def5ba02c4103d07963a5cda0c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-11 20:31:17 -0700

    new updates

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main -print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower and upper case
+ * 
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 97;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+
+}
