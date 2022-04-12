8b8f2455dc7c1d96a40dd6292ac722a4c62d2fbb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github>
2022-04-11 21:46:48 -0700

    new updates

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+/**
+ * main - printing 3-digit number combos
+ *
+ * Description: As shown above
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			k = j + 1;
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
