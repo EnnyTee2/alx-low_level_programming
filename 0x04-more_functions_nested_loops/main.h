271768dd3ceded77d5207247f254f5a349276ceb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-04-21 23:03:29 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,6 @@
                                                                                                                                                                  
 #include <stdio.h>                                                                                                                                               
                                                                                                                                                                  
-int _isupper(char c);                                                                                                                        
+int _isupper(int c);                                                                                                                        
                                                                                                                                                                  
 #endif /* MAIN_H */
