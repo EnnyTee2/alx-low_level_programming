8b251da028d588614cfa479af181351b3959458c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-04-21 22:58:58 +0100

    Create main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H                                                                                                                                                   
+#define MAIN_H                                                                                                                                                   
+                                                                                                                                                                 
+#include <stdio.h>                                                                                                                                               
+                                                                                                                                                                 
+int _isupper(char c);                                                                                                                        
+                                                                                                                                                                 
+#endif /* MAIN_H */
