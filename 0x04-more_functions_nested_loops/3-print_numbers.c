2acc4ea0272b7c329e6a1d03ffd0b32d2ff38a52
EnnyTee2 <trusteniola@gmail.com>
2022-04-24 08:56:39 -0700

    latest changes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers btw 0 and 9 inclusive
+ *
+ * Return: default
+ */
+
+void print_numbers()
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
