77d536f32abfbbf04fe27f4e254b72de33f5521e
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-04-24 19:59:49 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line with length proportional to the magnitude of n
+ * 
+ * Return: void
+ */
+
+void print_line(n)
+{
+        if (n < 1)
+        {
+                putchar('\n');
+        }
+        else
+        {
+                for (int i = 0; i <= n; i++)
+                {
+                        _putchar('_');
+                }
+                _putchar('\n');
+        }
+}
