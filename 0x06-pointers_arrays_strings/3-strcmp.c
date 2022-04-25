cc9a29ab731123026327d17bda52382701d33f54
EnnyTee2 <trusteniola@gmail.com>
2022-04-25 16:08:56 -0700

    updates

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@
 int _strcmp(char *s1, char *s2)
 {
         int i = 0, cmp = 0;
-        
-        while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
+	
+	while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
         {
-                cmp = s1[i] - s2[i];
-                i++;
-        }
-        
-        return (cmp);
+		cmp = s1[i] - s2[i];
+		i++;
+	}
+	
+	return (cmp);
 }
