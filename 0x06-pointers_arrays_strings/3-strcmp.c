484456177a6ed10fff3317e1738b56ee82dd7b03
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-04-26 00:20:29 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcmp - conpares two strings
+ * _strcmp - compares two strings
  * @s1: string one
  * @str2: string two
  * Return: int
@@ -9,7 +9,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0, cmp = 0;
+	int i = 0, cmp = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0' && cmp == 0)
 	{
