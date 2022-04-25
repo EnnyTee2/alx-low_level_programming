e6896a58c8299926e5f6d87a82eb6d76627690b5
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-04-25 23:49:55 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: A pointer to the second string to be compared.
  * @cmp: temp variable to hold comparison
  *
- * Return: cmp, the negative difference of the first unmatched characters.
+ * Return: int
  */
 
 int _strcmp(char *s1, char *s2)
