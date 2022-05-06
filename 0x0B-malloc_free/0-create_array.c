ff5bc67058ef003c3c61eb704d029a1f1e662460
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-06 21:31:44 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@
  char *create_array(unsigned int size, char c)
  {
         char *str;
-        str = malloc(sizeof(char));
+        str = malloc(sizeof(char)*size);
         str[0] = c;
         
         free(str);
