7bd24f9da0c48b79fc4a2efeafa80c4d83c9328b
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-10 00:02:59 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,28 +3,33 @@
 
 /**
  * _calloc - allocates memory for an array
- * @nmemb: number of members
- * @size: number of bytes
- * Return: pointer to allocated memory corresponding with the string
+ * @min: minimum, array start
+ * @max: maximum, array end
+ * Return: pointer to allocated memory corresponding with the array
  */
 
 int *array_range(int min, int max)
 {
 	void *arr;
 	unsigned int i;
-	char *k;
         int size = (max - min + 1);
+	char *k;
 
 	if (min > max)
+        {
 		return (NULL);
-
+        }
 	arr = malloc(size);
 	if (arr == NULL)
+        {
 		return (NULL);
-
+        }
 	k = arr;
+
 	for (i = 0; i < size; i++)
-		k[i] = min;
+        {
+                k[i] = min;
                 min++;
+        }
 	return (arr);
 }
