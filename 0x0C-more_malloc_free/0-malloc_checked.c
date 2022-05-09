821a3410536a61478dda7028fcc945f143f0fa6a
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-09 23:20:38 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,14 @@
  *
  * Return: A pointer to the allocated memory.
  */
- 
+
 void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
 
 	if (mem == NULL)
+	{
 		exit(98);
-
+	}
 	return (mem);
 }
