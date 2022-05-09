a41267800a66078d36ed5feeda048d9e54579918
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-05-09 23:36:20 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 
-**
+/**
  * _calloc - Allocates memory for an array of a certain number
  *           of elements each of an inputted byte size.
  * @nmemb: The number of elements.
@@ -10,7 +10,7 @@
  * Return: If nmemb = 0, size = 0, or the function fails - NULL.
  *         Otherwise - a pointer to the allocated memory.
  */
-         
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
         void *str;
@@ -30,6 +30,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
         {
                 ces[index] = 0;
         }
-        return (str);
-        
+        return (str);       
 }
