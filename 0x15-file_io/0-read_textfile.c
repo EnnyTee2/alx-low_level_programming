3cab1ad2b97e54bfd243771863e15a3e7e4b4993
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-03 02:25:18 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,46 @@
 #include "main.h"
 
 /**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
+ *
+ * Return: numbers of letters printed. If it fails, returns 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	close(fd);
+
+	free(buf);
+
+	return (wr)
+
+
+
+
+
+/* #include "main.h"
+
+/**
  * read_textfile - function that reads specified file
  * @readFile: holds the read file 
  * @writtenFile: holds the written data
@@ -8,7 +48,6 @@
  * @fd:	file descriptor
  *
  * Return: number of letters printed
- */
  
 ssize_t read_textfile(const char *filename, size_t letters)
 {	
@@ -42,3 +81,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	writtenFile = write(STD_FILENO, buff, readFile);
 	free(buff);
 }
+*/
