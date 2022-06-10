fc9059cb3a98f4735e000a490cffa0bf4e77a1ab
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-11 00:08:05 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  * read_textfile - function that reads specified file
- * @readFile: holds the read file 
+ * @readFile: holds the read file
  * @writtenFile: holds the written data
  * @buff: buffer for file storage
  * @fd:	file descriptor
@@ -11,36 +11,33 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-{	
+{
 	ssize_t readFile, fd, writtenFile;
 	char *buff;
-	
 	if (!filename)
 		return (0);
-	
 	buff = malloc(sizeof(char) * letters);
-	
+
 	if (!buff)
 		return (0);
-	
 	fd = open(filename, O_RDONLY);
 	readFile = read(fd, buff, letters);
 	writtenFile = write(STDOUT_FILENO, buff, readFile);
-	
+
 	if (fd == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	
+
 	if (readFile == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	
+
 	close(fd);
-	
+
 	free(buff);
 	return (writtenFile);
 }
