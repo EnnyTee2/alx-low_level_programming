f2dca203cfa14eec3f5c578033cdc9d6dba6f7ed
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-10 22:54:12 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,6 +21,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+	{
+		return (0);
+	}
 	
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -28,17 +33,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	
-	buff = malloc(sizeof(char) * letters);
-	if (!buff)
-	{
-		return (0);
-	}
 	readFile = read(fd, buff, letters);
 	if (readFile == -1)
 	{
 		return (0);
 	}
+	writtenFile = write(fd, buff, letters);
 	close(fd);
-	writtenFile = write(STDOUT_FILENO, buff, readFile);
+	
 	free(buff);
 }
