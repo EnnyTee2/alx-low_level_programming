35583033a864a8a016790e48215be8b8fcf5d0d5
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-10 22:24:54 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		retrun (0);
+		return (0);
 	}
 	
 	buff = malloc(sizeof(char) * letters);
