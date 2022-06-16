3ab8b746423f3a810f288fc1fbaea8881960dbeb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 19:33:41 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - print each character on a newline
+ * @str: string from which to print char
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+	int x = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	while (x < len)
+	{
+		_putchar(str[x]);
+		_putchar('\n');
+		x++;
+	}
+}
