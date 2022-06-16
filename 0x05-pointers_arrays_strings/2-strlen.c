25c5d38b0635efa3bbd602b3e445f804810c11fc
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 14:53:06 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,17 @@
 /**
  * _strlen - get the length of parsed string
  * @s: string to get length;
- * Return: returns the length 
+ * Return: returns the length
  */
 
 int _strlen(char *s)
 {
 	int x, count = 0;
-	
-	for (x = 0;; x++)
+
+	for (x = 0; ; x++)
 	{
-	    count++;
-	    if (s[x] == '\0')
+		count++;
+		if (s[x] == '\0')
 		break;
 	}
 	return (count - 1);
