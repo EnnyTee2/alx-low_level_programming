cc05a16952f201f54dbe89810a542485681d0d9c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 14:23:32 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function that resets
+ * @a: first integer to swap
+ * @b: second integer to swap
+ * @temp: temporary variable for swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
