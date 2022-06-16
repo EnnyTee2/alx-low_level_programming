1b4864a7d15f337c332eb2f11f4b8ce7cd444907
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 20:32:53 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input string 
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, half;
+	
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	
+	if (len % 2 == 0)
+	{
+		half = len / 2;
+		while (half < len)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		half = (len - 1) / 2;
+		while (half < len)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+		_putchar('\n');
+		
+	}
+}
