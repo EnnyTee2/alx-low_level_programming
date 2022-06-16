80c3b3e4f126f19d0834f8e680522a65c7a01ac8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 15:25:43 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,22 @@
  * Return: void
  */
 
+int _strlength(char *a)
+{
+	int x, count = 0;
+
+	for (x = 0; ; x++)
+	{
+		count++;
+		if (a[x] == '\0')
+		break;
+	}
+	return (count - 1);
+}
+
 void print_rev(char *s)
 {
-	int x, len = _strlen(s);
+	int x, len = _strlength(s);
 
 	for (x = len; x >= 0; x--)
 	{
