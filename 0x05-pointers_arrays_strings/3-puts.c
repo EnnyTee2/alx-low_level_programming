18cc9fccda74931834bc2d2a206a221d82fc39ba
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-16 15:03:55 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - write a string to stdout
+ * @str: string to write
+ * Return: returns the length
+ */
+
+void _puts(char *str)
+{
+	int c = 0;
+
+	while(str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+
+	_putchar('\n');
+}
