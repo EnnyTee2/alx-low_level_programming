7f49b52387ceb61e00e2ed11dd0d94e1bf20d00d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-17 10:32:50 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,20 +2,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-
-/**
- * print_c - Prints char
- * @arg: A list of arguments pointing to
- *       the character to be printed.
- * Return: void
- */
-
 void print_char(va_list arg);
 void print_int(va_list arg);
 void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
+/**
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
+ */
 
 void print_char(va_list arg)
 {
@@ -25,6 +22,11 @@ void print_char(va_list arg)
 	printf("%c", letter);
 }
 
+/**
+ * print_int - Prints an int.
+ * @arg: A list of arguments pointing to
+ *       the integer to be printed.
+ */
 
 void print_int(va_list arg)
 {
@@ -34,6 +36,12 @@ void print_int(va_list arg)
 	printf("%d", num);
 }
 
+/**
+ * print_float - Prints a float.
+ * @arg: A list of arguments pointing to
+ *       the float to be printed.
+ */
+
 void print_float(va_list arg)
 {
 	float num;
@@ -41,6 +49,12 @@ void print_float(va_list arg)
 	num = va_arg(arg, double);
 	printf("%f", num);
 }
+
+/**
+ * print_string - Prints a string.
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
+ */
 
 void print_string(va_list arg)
 {
@@ -66,6 +80,7 @@ void print_string(va_list arg)
  *              or char * is ignored.
  *              If a string argument is NULL, (nil) is printed instead.
  */
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
