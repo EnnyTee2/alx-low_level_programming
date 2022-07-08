87854f9e4f5dbbcc05c0f286bf60946df5744a76
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-08 20:25:30 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	
+
 	return (nodes);
 }
