bd378f45f818b9fda8956ec710d3b409dc3d8b58
EnnyTee2 <trusteniola@gmail.com>
2022-07-12 14:52:00 -0700

    updates

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - function to cocantenate characters
+ * @dest: destination address
+ * @src: source address
+ * Return: returns zero
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (0);
+}
