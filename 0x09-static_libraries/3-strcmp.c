cc730297286ad0f80263355c53d04738f5a79ecc
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 21:36:06 +0100

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,3 +1,11 @@
+/**
+ * _strcmp - main function
+ * @s1: 1st variable to compare
+ * @s2: second variable to compare
+ * Return: returns zero
+ */
+
+
 int _strcmp(char *s1, char *s2)
 {
         return (0);
