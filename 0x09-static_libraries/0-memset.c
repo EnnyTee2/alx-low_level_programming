1a06b49f403343a655ffd71638c97648bb82336f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 22:11:11 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,4 +1,10 @@
-#include "main.h"
+/**
+ * _memset - main function
+ * @s: variable to set
+ * @b: 2nd variable to process
+ * @n: integer n
+ * Return: returns zero
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
