eb699f87ab94b778f63eb6e9bfc2a79b50384eeb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 21:19:01 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,4 +1,11 @@
+/**
+ * _strchr - converts string to char
+ * @s: variable to process
+ * @c: character variable
+ * Return: returns zero
+ */
+
 char *_strchr(char *s, char c)
 {
-        return (0);
+	return (0);
 }
