695b59eee7233abd9cc7730dfad36dc1921ed2c0
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 21:27:41 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,4 +1,12 @@
+/**
+ * _strncpy - main function
+ * @dest: destination address
+ * @src: source address
+ * @n: integer
+ * Return: returns zero
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-        return (0);
+	return (0);
 }
