d07a500ea4323ed92451c39cc8811ce8404261e2
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 21:40:05 +0100

    Update 4-strpbrk.c

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,4 +1,11 @@
+/**
+ * _strpbrk - main function
+ * @s: variable to process
+ * @accept: 2nd variable to process
+ * Return: returns zero
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
-        return (0);
+	return (0);
 }
