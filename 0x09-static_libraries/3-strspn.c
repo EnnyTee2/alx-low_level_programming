47f90bfd8a2d7bc55cbd038b5fb32b5577e93f34
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 21:37:34 +0100

    Update 3-strspn.c

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,3 +1,10 @@
+/**
+ * _strspn - main function
+ * @s: variable to process
+ * @accept: second variable
+ * Return: returns zero
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
         return (0);
