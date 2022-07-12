4cd8550daee0b6fe6b3b42aea93dc9c63b4628de
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-12 23:09:57 +0100

    Update 1-strncat.c

--- 0x18-dynamic_libraries/1-strncat.c
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -4,10 +4,11 @@
  * _strncat - function to cocantenate characters
  * @dest: destination address
  * @src: source address
+ * @n: integer n
  * Return: returns zero
  */
 
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	return (0);
 }
