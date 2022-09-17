c4a90c1375d27575abe03c9a9cee22d48ecf7b91
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-17 22:21:46 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,17 +3,28 @@ def island_perimeter(grid):
     """this function returns the perimeter
     of the island described in grid:"""
     perimeter = 0
-    glen = len(grid) - 1
-    zlen = len(grid[0]) - 1
-    for i in range(1, glen):
-        for j in range(1, zlen):
-            if grid[i][j] == 1:
-                if grid[i][j+1] == 0:
-                    perimeter += 1
-                if grid[i][j-1] == 0:
-                    perimeter += 1
-                if grid[i+1][j] == 0:
-                    perimeter += 1
-                if grid[i-1][j] == 0:
-                    perimeter += 1
+    glen = len(grid)
+    zlen = len(grid[0])
+    
+    for i in range(0, glen):
+        if i == 0 or i == glen - 1:# top or bottom border
+            for j in range(0, zlen):
+                if grid[i][j] == 1:# land found on border
+                    return 0
+        else:
+            for j in range(0, zlen):
+                if j == 0 or j == zlen - 1:# left or right border
+                    if grid[i][j] == 1:# land found on border
+                        return 0
+                else:
+                    if grid[i][j] == 1:# land found in cell
+                        # The if blocks below detects if the cell is surrounded by water
+                        if grid[i][j+1] == 0:
+                            perimeter += 1
+                        if grid[i][j-1] == 0:
+                            perimeter += 1
+                        if grid[i+1][j] == 0:
+                            perimeter += 1
+                        if grid[i-1][j] == 0:
+                            perimeter += 1
     return perimeter
