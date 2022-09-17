f2665688e06cf436ef9384d9adae3c73c8d0e65e
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-17 21:00:22 +0100

    Create 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+    
+def island_perimeter(grid):
+    """this function returns the perimeter
+    of the island described in grid:"""
+    perimeter = 0
+    glen = len(grid) - 1
+    zlen = len(grid[0]) - 1
+    for i in range(1, glen):
+        for j in range(1, zlen):
+            if grid[i][j] == 1:
+                if grid[i][j+1] == 0:
+                    perimeter += 1
+                if grid[i][j-1] == 0:
+                    perimeter += 1
+                if grid[i+1][j] == 0:
+                    perimeter += 1
+                if grid[i-1][j] == 0:
+                    perimeter += 1
+    return perimeter
